2d5fe7d7c5819cafe519cf6884d9fbe3bcd80f31
Medvidek77 <121556205+Medvidek77@users.noreply.github.com>
2023-12-03 11:48:34 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Bot is ready")
+
+@bot.event
+async def on_member_leave(member):
+    await asyncio.sleep(5)
+    user = bot.get_user(member.id)
+    await user.send("Hello there!")
+
+bot.run("MTE4MDgxODE5ODY1Njk4NzE2Ng.G7GCxO.l_wsKH8Zhj5qD3QaZgAHDcPTFjwQv2yx0p9-Ks")
