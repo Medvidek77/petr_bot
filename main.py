408b77809707cbd7aa2da0934e8ec364684bfcd3
Medvidek77 <121556205+Medvidek77@users.noreply.github.com>
2023-12-08 19:00:51 +0100

    3

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ async def on_ready():
     print("Bot is ready")
 
 @bot.event
-async def on_message(member):
-    if member.id == 973567020903858186:
-        try:
-            user = bot.get_user(member.id)
-            await user.send("Hello there!")
-        except Exception as e:
-            print(e)
+async def on_message(message):
+    
+    try:
+        user = bot.get_user(message.author.id)
+        await user.send("Hello there!")
+    except Exception as e:
+        print(e)
 
 bot.run("MTE4MjczMzE1MTAxODE2MDI4OA.GxoicZ.cDvtz4hV8OwuHiG1uvuHzZK2oym86JsauNLn8I")
