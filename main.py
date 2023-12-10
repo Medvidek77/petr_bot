a4e5ef49f25bc4a02a265ea035b284d150239f57
Medvidek77 <121556205+Medvidek77@users.noreply.github.com>
2023-12-10 19:23:07 +0100

    13

--- main.py
+++ main.py
@@ -13,16 +13,8 @@ async def on_ready():
 async def on_member_remove(member):
 
     text1 = f"""
-    Hi there {member.mention}! :wave:\n\n
-
-    I hope you're doing great! I've noticed you recently joined our Discord group. :blush:\n
-    I'm reaching out to share something exciting with you about our course. It dives deep into key areas like technical\nanalysis, market structure, Fibonacci retracement, and Exocharts. :bar_chart: This course is made to boost your trading\nskills with practical knowledge. Plus, purchasing it grants you exclusive access to coaches' channels,\nincluding\nJan's\nexpert setups and insights, along with channels dedicated to our wizard strategy.\n
-    I believe this course, along with our dedicated Discord group, could be the perfect fit for you! :star2:\n\n
-    Interested in learning more? Check out the details here:\n
-    CW-Trading: [https://cw-trading.cz/](https://cw-trading.cz/)\n
-    Crypto Wizards: [https://crypto-wizards.net/](https://crypto-wizards.net/)\n\n
-    Feel free to reach out to CW_Admin in DMs if you have any questions. :raised_hands:\n\n
-    Looking forward to hearing from you soon!
+    Hi there {member.mention}! :wave:\n\nI hope you're doing great! I've noticed you recently joined our Discord group. :blush:\nI'm reaching out to share something exciting with you about our course. It dives deep into key areas like technical\nanalysis, market structure, Fibonacci retracement, and Exocharts. :bar_chart: This course is made to boost your trading\nskills with practical knowledge. Plus, purchasing it grants you exclusive access to coaches' channels,\nincluding\nJan's\nexpert setups and insights, along with channels dedicated to our wizard strategy.\nI believe this course, along with our dedicated Discord group, could be the perfect fit for you! :star2:\n\n
+    Interested in learning more? Check out the details here:\nCW-Trading: [https://cw-trading.cz/](https://cw-trading.cz/)\nCrypto Wizards: [https://crypto-wizards.net/](https://crypto-wizards.net/)\n\nFeel free to reach out to CW_Admin in DMs if you have any questions. :raised_hands:\n\nLooking forward to hearing from you soon!
     """
 
     text2 = """
