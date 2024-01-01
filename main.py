d48ee5cc87e435e90b97088b54c3ff57b817ccbe
Medvidek77 <121556205+Medvidek77@users.noreply.github.com>
2024-01-01 16:25:42 +0100

    142

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ Since you’re only in the free version, information and education are very limi
 > * Risk Management
 > * and more…
 
-Membership includes: :white_tick:
+Membership includes: :white_check_mark:
 
 * Exocharts Pro license worth 2500 CZK
 * Daily CW team analyses
@@ -53,7 +53,7 @@ Všiml jsem si, že jsi se nedávno připojil k naší CW Trading skupině. Douf
 > * Risk Management
 > * a další…
 
-Součástí členství je: :white_tick:
+Součástí členství je: :white_check_mark:
 
 * Exocharts Pro licence v hodnotě 2500 ,- Kč
 * Denní analýzy CW týmu
