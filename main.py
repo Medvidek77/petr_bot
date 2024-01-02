5f219800fe9f40c74718d68397c97b8faa9c310e
Medvidek77 <121556205+Medvidek77@users.noreply.github.com>
2024-01-02 19:31:31 +0100

    150

--- main.py
+++ main.py
@@ -68,9 +68,8 @@ Dostaneš 5% slevu na kurz s kódem “SPECTATOR5”, který můžeš zadat při
     embed.set_image(url="https://cdn.discordapp.com/attachments/1180556010465800225/1184133248440356934/cw_logo_kopie.png?ex=658add06&is=65786806&hm=bcf39cc82e93e6c9ef519494b7ac83d850a9ed715d76605109844e860d69109a&")
     
     try:
-        await asyncio.sleep(10)
-        if "SP" in [role.name for role in member.roles]: 
-            await asyncio.sleep(10)
+        await asyncio.sleep(345600)
+        if "Spectator" in [role.name for role in member.roles] and member.bot == False and "Wizard" not in [role.name for role in member.roles] and "Wizards plus" not in [role.name for role in member.roles]:
             await member.send(embed=embed)
         else: 
             pass
@@ -78,14 +77,15 @@ Dostaneš 5% slevu na kurz s kódem “SPECTATOR5”, který můžeš zadat při
         print(f"Error: {e}")
 
 
-
+# 1177613374155137027
 
 @bot.event
 async def on_message(message):
     try:
         if message.author.bot:
             return
-        elif message.content.startswith("!dm ") or message.content.startswith("!dm") or message.content.startswith("!dm  ") or message.content.startswith("!dm \n") or message.content.startswith("!dm \n\n") or message.content.startswith("!dm \n\n\n") or message.content.startswith("!dm \n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n\n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n\n\n\n\n\n") or message.content.startswith("!dm\n"):
+
+        elif message.author.id == 1177613374155137027 and message.content.startswith("!dm ") or message.content.startswith("!dm") or message.content.startswith("!dm  ") or message.content.startswith("!dm \n") or message.content.startswith("!dm \n\n") or message.content.startswith("!dm \n\n\n") or message.content.startswith("!dm \n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n\n\n\n\n") or message.content.startswith("!dm \n\n\n\n\n\n\n\n\n\n") or message.content.startswith("!dm\n"):
             
             text = message.content.split("!dm ", 1)[1]
             await message.delete()
@@ -102,8 +102,8 @@ async def on_message(message):
                     for member in all_members:
                         if member.bot:
                             pass
-                        #elif member == message.author:
-                        #    pass
+                        elif member == message.author:
+                            pass
                         else:
                             try:
                                 await member.send(text)
@@ -121,6 +121,128 @@ async def on_message(message):
                     await message_cancel.delete()
 
             await message.channel.send(f"Verify your message before send!\n\n**Text:**\n{text}", view=MyView())
+
+
+        elif message.author.id == 1177613374155137027 and message.content.startswith("!dmSP") or message.content.startswith("!dmSP ") or message.content.startswith("!dmSP  ") or message.content.startswith("!dmSP \n") or message.content.startswith("!dmSP \n\n") or message.content.startswith("!dmSP \n\n\n") or message.content.startswith("!dmSP \n\n\n\n") or message.content.startswith("!dmSP \n\n\n\n\n") or message.content.startswith("!dmSP \n\n\n\n\n\n") or message.content.startswith("!dmSP \n\n\n\n\n\n\n") or message.content.startswith("!dmSP \n\n\n\n\n\n\n\n") or message.content.startswith("!dmSP \n\n\n\n\n\n\n\n\n") or message.content.startswith("!dmSP\n"):
+            text = message.content.split("!dmSP ", 1)[1]
+            await message.delete()
+
+            class MyView(discord.ui.View): 
+                @discord.ui.button(label="Send", style=discord.ButtonStyle.success)
+                async def send_button(self, button, interaction):
+                    button.disabled = True
+                    message_send = await message.channel.send("Sending DMs to all members **with Spectator role**")
+                    all_members = message.guild.members
+                    await asyncio.sleep(2)
+                    await message_send.delete()
+                    await interaction.message.delete()
+                    for member in all_members:
+                        if member.bot:
+                            pass
+                        elif member == message.author:
+                            pass
+                        elif "Spectator" in [role.name for role in member.roles]:
+                            try:
+                                await member.send(text)
+                            except Exception as e:
+                                print(f"Error: {e}")
+                                pass
+                        else:
+                            pass
+                            
+
+                @discord.ui.button(label="Cancel", style=discord.ButtonStyle.danger)
+                async def cancel_button(self, button, interaction):
+                    button.disabled = True
+                    message_cancel = await message.channel.send("Canceled")
+                    await asyncio.sleep(2)
+                    await interaction.message.delete()
+                    await message_cancel.delete()
+
+
+
+            await message.channel.send(f"Verify your message before send!\n\n**Text:**\n{text}", view=MyView()) 
+        
+        elif message.author.id == 1177613374155137027 and message.content.startswith("!dmW") or message.content.startswith("!dmW ") or message.content.startswith("!dmW  ") or message.content.startswith("!dmW \n") or message.content.startswith("!dmW \n\n") or message.content.startswith("!dmW \n\n\n") or message.content.startswith("!dmW \n\n\n\n") or message.content.startswith("!dmW \n\n\n\n\n") or message.content.startswith("!dmW \n\n\n\n\n\n") or message.content.startswith("!dmW \n\n\n\n\n\n\n") or message.content.startswith("!dmW \n\n\n\n\n\n\n\n") or message.content.startswith("!dmW \n\n\n\n\n\n\n\n\n") or message.content.startswith("!dmW\n"):
+            text = message.content.split("!dmW ", 1)[1]
+            await message.delete()
+
+            class MyView(discord.ui.View): 
+                @discord.ui.button(label="Send", style=discord.ButtonStyle.success)
+                async def send_button(self, button, interaction):
+                    button.disabled = True
+                    message_send = await message.channel.send("Sending DMs to all members **with Wizard role**")
+                    all_members = message.guild.members
+                    await asyncio.sleep(2)
+                    await message_send.delete()
+                    await interaction.message.delete()
+                    for member in all_members:
+                        if member.bot:
+                            pass
+                        elif member == message.author:
+                            pass
+                        elif "Wizard" in [role.name for role in member.roles]:
+                            try:
+                                await member.send(text)
+                            except Exception as e:
+                                print(f"Error: {e}")
+                                pass
+                        else:
+                            pass
+                            
+
+                @discord.ui.button(label="Cancel", style=discord.ButtonStyle.danger)
+                async def cancel_button(self, button, interaction):
+                    button.disabled = True
+                    message_cancel = await message.channel.send("Canceled")
+                    await asyncio.sleep(2)
+                    await interaction.message.delete()
+                    await message_cancel.delete()
+
+
+
+            await message.channel.send(f"Verify your message before send!\n\n**Text:**\n{text}", view=MyView())
+
+        elif message.author.id == 1177613374155137027 and message.content.startswith("!dmWP") or message.content.startswith("!dmWP ") or message.content.startswith("!dmWP  ") or message.content.startswith("!dmWP \n") or message.content.startswith("!dmWP \n\n") or message.content.startswith("!dmWP \n\n\n") or message.content.startswith("!dmWP \n\n\n\n") or message.content.startswith("!dmWP \n\n\n\n\n") or message.content.startswith("!dmWP \n\n\n\n\n\n") or message.content.startswith("!dmWP \n\n\n\n\n\n\n") or message.content.startswith("!dmWP \n\n\n\n\n\n\n\n") or message.content.startswith("!dmWP \n\n\n\n\n\n\n\n\n") or message.content.startswith("!dmWP\n"):
+            text = message.content.split("!dmWP ", 1)[1]
+            await message.delete()
+
+            class MyView(discord.ui.View): 
+                @discord.ui.button(label="Send", style=discord.ButtonStyle.success)
+                async def send_button(self, button, interaction):
+                    button.disabled = True
+                    message_send = await message.channel.send("Sending DMs to all members **with Wizard Plus role**")
+                    all_members = message.guild.members
+                    await asyncio.sleep(2)
+                    await message_send.delete()
+                    await interaction.message.delete()
+                    for member in all_members:
+                        if member.bot:
+                            pass
+                        elif member == message.author:
+                            pass
+                        elif "Wizards plus" in [role.name for role in member.roles]:
+                            try:
+                                await member.send(text)
+                            except Exception as e:
+                                print(f"Error: {e}")
+                                pass
+                        else:
+                            pass
+                            
+
+                @discord.ui.button(label="Cancel", style=discord.ButtonStyle.danger)
+                async def cancel_button(self, button, interaction):
+                    button.disabled = True
+                    message_cancel = await message.channel.send("Canceled")
+                    await asyncio.sleep(2)
+                    await interaction.message.delete()
+                    await message_cancel.delete()
+
+
+
+            await message.channel.send(f"Verify your message before send!\n\n**Text:**\n{text}", view=MyView())
+
     except Exception as e:
         print(f"An error occurred: {e}")
 
